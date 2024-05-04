fefd807fa916479e1e70722bf63c0c8af34e7d68
Marine0105 <mwilashapi8@gmail.com>
2024-05-04 02:57:52 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express  from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -11,3 +12,8 @@ mongoose.connect(
 const app = express();
 app.listen(3000,()=>{console.log('listening on 3000');
 });
+
+  
+
+app.use('/api/user', userRoutes);
+  
